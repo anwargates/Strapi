c48e7ab8d9d359beb1f9cf6c252b43f1c2d912cc
anwargates <102929799+anwargates@users.noreply.github.com>
2022-12-06 11:32:02 +0700

    preview 1

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,25 +1,34 @@
 module.exports = [
-  'strapi::errors',
+  "strapi::errors",
+  // "strapi::security",
   {
-    name: 'strapi::security',
+    name: "strapi::security",
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  // {
+  //   name: "strapi::cors",
+  //   config: {
+  //     // enabled: true,
+  //     // headers: "*",
+  //     origin: "http://192.168.10.106:1337",
+  //   },
+  // },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
